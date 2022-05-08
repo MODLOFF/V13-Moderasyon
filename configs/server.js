5a910ac1aca42f35125474c13b84cab58455b6f3
Mustafa Can <57722514+MODLOFF@users.noreply.github.com>
2022-05-08 18:03:52 +0300

    Update server.js

--- configs/server.js
+++ configs/server.js
@@ -2,8 +2,8 @@ module.exports = {
     embedColor: "RANDOM",
     admins: ["920033643190186006"], //! BOT OWNER ID
     serverId: "965159582966108180", //! BOT YAPTIĞINIZ SUNUCU ID
-    token: "OTY4MjYyODA1Mjk3OTU4OTUz.YmcTAg.U1ZY8PA8zcdAAf_Aw8qCc5n7qKk", //! BOT TOKENİ
-    mongoURL: "mongodb+srv://passengerxdd:passengerxdd@cluster0.l8zqh.mongodb.net/denemebotsh?retryWrites=true&w=majority", //! MONGO DB LİNKİ https://www.mongodb.com/
+    token: "", //! BOT TOKENİ
+    mongoURL: "", //! MONGO DB LİNKİ https://www.mongodb.com/
     status: ["MODLOFF", "V13 Video Bot"], //! BOT OYNUYOR KISMI
     muteLimit: 10,
     banLimit: 5,
